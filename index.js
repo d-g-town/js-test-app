38467e6de540bb216367e17289bdba4f2c7af11b
d-g-town <66391417+d-g-town@users.noreply.github.com>
2025-10-23 14:26:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  logJSON("info", "Root endpoint accessed", {
+  logJSON("info", "Root endpoint accessed...", {
     route: {
       path: "/",
       method: "GET",
